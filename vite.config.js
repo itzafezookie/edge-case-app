ca7c52e6addd0c1ce54fe007343d4986ea556d17
itzafezookie <brendan.fezuk@outlook.com>
2026-02-23 11:48:02 -0500

    Configure PWA manifest and service worker

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/edge-case-app/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['EC_Icon.png'],
+      manifest: {
+        name: 'EdgeCase',
+        short_name: 'EdgeCase',
+        description: 'A Big Cube Reference App',
+        theme_color: '#2f2f2f',
+        icons: [
+          {
+            src: 'EC_Icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
